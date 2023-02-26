0482e0fc55ea49265d65bc0d201a9e1cb5333a87
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-02-26 11:46:07 +0000

    Deploying to gh-pages from @ KalinichenkoArtem/goit-js-hw-09@673dff511d3d19d1a4a36585ba923bc198d2c9de 🚀

--- 01-color-switcher.1f658a6f.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0,o.addEventListener("click",(function(){o.disabled=!0,e.disabled=!1;const t=document.body;changeColor=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),console.log("Start change color")})),e.addEventListener("click",(function(){o.disabled=!1,e.disabled=!0,clearInterval(changeColor),console.log("Stop change color")}));
-//# sourceMappingURL=01-color-switcher.1f658a6f.js.map
