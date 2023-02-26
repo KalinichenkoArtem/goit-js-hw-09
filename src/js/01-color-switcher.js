dae3d481808cf59e00669c506e3de54dd5d5cb82
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-02-26 15:48:31 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,27 +3,24 @@ const btnStop = document.querySelector('[data-stop]');
 
 btnStop.disabled = true;
 
-btnStart.addEventListener('click', onBtnStartClick);
-btnStop.addEventListener('click', onBtnStopClick);
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-function onBtnStartClick() {
+btnStart.addEventListener('click', () => {
   btnStart.disabled = true;
   btnStop.disabled = false;
   const body = document.body;
 
-changeColor = setInterval(() => {
+  changeColor = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
   console.log("Start color change");
-};
+});
 
-function onBtnStopClick() {
-  btnStart.disabled = false;
+btnStop.addEventListener('click', () => {
+   btnStart.disabled = false;
   btnStop.disabled = true;
   clearInterval(changeColor);
   console.log("Stop color change");
-};
+});
