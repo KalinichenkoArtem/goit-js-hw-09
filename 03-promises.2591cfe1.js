8450dfea7fc751d8e21ad2aa160a56c8a192bb43
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2023-03-11 08:51:12 +0000

    Deploying to gh-pages from @ KalinichenkoArtem/goit-js-hw-09@6aafc1ee6cf4dbf3e1161d483e49885b96b612ed 🚀

--- 03-promises.2591cfe1.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("submit",void Math.random());
-//# sourceMappingURL=03-promises.2591cfe1.js.map
--- 03-promises.efdb6c9b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button").addEventListener("submit",void Math.random());
-//# sourceMappingURL=03-promises.efdb6c9b.js.map
